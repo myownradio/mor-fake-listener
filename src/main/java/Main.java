6a92cb2c0d097c6dbf4a34fd6dedc7d5b792d4fe
Roman Gemini <roman@homefs.biz>
2017-02-08 11:46:47 +0200

    Fix props.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,17 @@ import service.entities.ChannelEntry;
 import service.entities.JSONResponse;
 import service.listener.Client;
 import service.listener.ClientSession;
-import tools.Props;
 import tools.ThreadTool;
 
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by Roman on 04.05.2015.
- */
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        int max = Props.getPropertyAsIntegerOrFail("listeners.max");
-
-        final ThreadPoolExecutor executorService = new ThreadPoolExecutor(max, max, 0L, TimeUnit.MILLISECONDS,
+        final ThreadPoolExecutor executorService = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS,
                 new LinkedBlockingQueue<>(8));
 
         executorService.setRejectedExecutionHandler((r, executor) -> {
@@ -45,20 +39,16 @@ public class Main {
         JobController controller = new JobController(executorService);
 
         while (!Thread.currentThread().isInterrupted()) {
-            Thread thread = new Thread(() -> {
-                JSONResponse response = ChannelsList.getRandomChannel();
-                ChannelEntry entry = response.getData();
-                Client client = new Client() {{
-                    this.setChannelId(entry.getSid());
-                    this.setListeningTime((long) (Math.random() * 7_200_000L));
-                }};
-                ClientSession session = new ClientSession(client);
-                System.out.println(entry.getName() + " will be listened for " + (client.getListeningTime() / 1000) + " seconds");
-                executorService.submit(session);
-                ThreadTool.sleep((long) (Math.random() * 60_000L));
-            });
-            thread.start();
-            thread.join();
+            JSONResponse response = ChannelsList.getRandomChannel();
+            ChannelEntry entry = response.getData();
+            Client client = new Client() {{
+                this.setChannelId(entry.getSid());
+                this.setListeningTime((long) (Math.random() * 7_200_000L));
+            }};
+            ClientSession session = new ClientSession(client);
+            System.out.println(entry.getName() + " will be listened for " + (client.getListeningTime() / 1000) + " seconds");
+            executorService.submit(session);
+            ThreadTool.sleep((long) (Math.random() * 60_000L));
         }
     }
 }
